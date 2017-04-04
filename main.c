e83178730ea956849e1aa26dcb8fa2bbcdf69101
Tornom <lucasalbao@gmail.com>
2017-04-04 20:17:09 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d;//criando as variaveis necessarias
+    printf("Digite uma senha\n");// pede a senha
+    scanf("%d %d",&a,&b);// configura o valor da senha
+
+    printf("Digite a senha\n"); //pede a senha novamente
+    scanf("%d %d",&c,&d); // guarda o valor da senha
+
+    if (a==c && b==d ){ //compara a senha correta com a senha obtida
+        printf("Cofre aberto"); //o cofre abriu
+
+    }
+    else{
+        printf("Senha incorreta");  // o usuario errou a senha
+    }
+    return 0;
+}
